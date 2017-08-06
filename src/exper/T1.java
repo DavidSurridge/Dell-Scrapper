47a81e0b69be64aff9a1759c21a069490fca5770
DavidSurridge <davidsurridge@gmail.com>
2017-08-06 23:29:53 +0100

    refinements and updates

--- src/exper/T1.java
+++ src/exper/T1.java
@@ -1,17 +1,22 @@
 package exper;
 
-import java.net.*;
 import java.util.List;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
+
 import org.jsoup.select.Elements;
 
-import java.io.*;
-
 public class T1 {
+
+	public static String getStringContaining(String start, String end, String string) {
+		if (string.indexOf(start) != -1) {
+			string = string.substring(string.indexOf(start) + start.length());
+			string = string.substring(0, string.indexOf(end));
+			return string;
+		}
+		return null;
+	}
 
 	public static void main(String[] args) throws Exception {
 
@@ -27,22 +32,54 @@ public class T1 {
 				Elements laptopModels = response.getElementsByClass("configStackBody highlightSpecs");
 				Element name = response.getElementById("mastheadPageTitle");
 
-				if (!name.toString().contains("Alienware")) {
-					for (Element laptop : laptopModels) {
-						Elements singles = laptop.children();
-						for (Element single : singles) {
-							// figure out better way of extracting children
-							System.out.println(name.text());
+				for (Element laptop : laptopModels) {
+					Elements singles = laptop.children();
+					// figure out better way of extracting children
+					for (Element single : singles) {
+
+						if (!name.toString().contains("Alienware")) {
+							// CPU
+							String CPU = single.getElementsByAttributeValue("data-specindex", "1").eachText().get(0);
+							CPU = getStringContaining("Core™ ", " ", CPU);
 							
+							// Ram							
+							String RAM = single.getElementsByAttributeValue("data-specindex", "7").eachText().get(0);
+							RAM = getStringContaining("", "z", RAM);
+							System.out.println(name.text());
+							System.out.println(CPU);
+							System.out.println(RAM + "z");
+							
+							// SSD/HDD
+							String diskDescription = single.getElementsByAttributeValue("data-specindex", "9").eachText().get(0);
+							boolean SSD = diskDescription.contains("Solid State Drive");
+							boolean HDD = diskDescription.contains("Hard Drive");
+							String drives[] = diskDescription.split(" \\+ ");
+							for (String drive : drives) {
+								// mapper to add each drive, size and type
+								String size = getStringContaining("", " ", drive);
+							}
+							System.out.println("SSD: " + SSD + " HDD: " + HDD);
+							// Graphics or Alienware SSD
 							System.out.println(
-									single.getElementsByAttributeValue("data-specindex", "7").eachText().get(0));
+									single.getElementsByAttributeValue("data-specindex", "13").eachText().get(0));
+							// screen
 							System.out.println(
-									single.getElementsByAttributeValue("data-specindex", "9").eachText().get(0));
+									single.getElementsByAttributeValue("data-specindex", "15").eachText().get(0));
+							// price
+							System.out.println(single.getElementsByClass("pLine dellPrice").eachText().get(0));
+							System.out.println(" ");
+						} else {
+							System.out.println(name.text());
+							System.out.println(
+									single.getElementsByAttributeValue("data-specindex", "1").eachText().get(0));
+							System.out.println(
+									single.getElementsByAttributeValue("data-specindex", "11").eachText().get(0));
 							System.out.println(
 									single.getElementsByAttributeValue("data-specindex", "13").eachText().get(0));
 							System.out.println(
-									single.getElementsByAttributeValue("data-specindex", "15").eachText().get(0));
-							System.out.println(single.getElementsByClass("pLine dellPrice").eachText().get(0));
+									single.getElementsByAttributeValue("data-specindex", "5").eachText().get(0));
+							System.out.println(
+									single.getElementsByAttributeValue("data-specindex", "9").eachText().get(0));
 							System.out.println(" ");
 						}
 					}
